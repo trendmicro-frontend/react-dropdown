c61212509f720654e7113ddb006c7296f19d1719
Cheton Wu <cheton@gmail.com>
2017-03-09 15:04:52 +0800

    Update docs

--- docs/bundle.js
+++ docs/bundle.js
@@ -945,7 +945,7 @@ exports = module.exports = __webpack_require__("../node_modules/css-loader/lib/c
 
 
 // module
-exports.push([module.i, ".fa {\n  margin-right: 8px;\n}\ni.fa,\nspan.fa {\n  font-size: 14px;\n  color: #666;\n}\n.section-group---qk5Ty {\n  margin-bottom: 12px;\n}\n.input-checkbox---JA88y {\n  vertical-align: middle;\n  margin-right: 5px;\n  margin-bottom: 2px;\n}\n.dropdown-example---2GyVl .dropdown-menu---1pkAo {\n  clear: left;\n  position: static;\n}\n.dropdown-example---2GyVl .dropdown-menu---1pkAo .badge---1_wqL {\n  float: right;\n  margin-left: 24px;\n  margin-top: 1px;\n}\n.dropdown-filter---1R3Yj > .dropdown-menu---1pkAo {\n  padding: 0;\n}\n.dropdown-menu-filter---3sZdC {\n  margin: 8px;\n}\n.dropdown-menu---1pkAo .checkbox---1GXiZ > label {\n  width: 100%;\n}\ninput[type=\"text\"]:focus,\ninput[type=\"number\"]:focus,\ntextarea:focus,\n.input-focus---1oxQl {\n  border-color: #0096cc;\n  -webkit-transition: all 0.2s linear 0s;\n  -moz-transition: all 0.2s linear 0s;\n  -o-transition: all 0.2s linear 0s;\n  -ms-transition: all 0.2s linear 0s;\n  transition: all 0.2s linear 0s;\n  outline: none;\n}\n.input-icon-group---3IKaZ {\n  position: relative;\n}\n.input-icon-group---3IKaZ > .form-control---1sMvt {\n  padding-left: 30px;\n  padding-right: 36px;\n}\n.input-icon-label---1ZNi_ {\n  position: absolute;\n  left: 9px;\n  top: 50%;\n  margin-top: -10px;\n  color: #666;\n}\n.form-control---1sMvt {\n  display: block;\n  width: 100%;\n  padding: 5px 12px;\n  font-size: 13px;\n  color: #222;\n  background-color: #fff;\n  background-image: none;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n}\n", ""]);
+exports.push([module.i, ".fa {\n  margin-right: 8px;\n}\ni.fa,\nspan.fa {\n  font-size: 14px;\n  color: #666;\n}\n.section-group---qk5Ty {\n  margin-bottom: 12px;\n}\n.input-checkbox---JA88y {\n  vertical-align: middle;\n  margin-right: 5px;\n  margin-bottom: 2px;\n}\n.dropdown-example---2GyVl .dropdown-menu---1pkAo {\n  clear: left;\n  position: static;\n}\n.dropdown-example---2GyVl .dropdown-menu---1pkAo .badge---1_wqL {\n  float: right;\n  margin-left: 24px;\n  margin-top: 1px;\n}\n.dropdown-filter---1R3Yj > .dropdown-menu---1pkAo {\n  padding: 0;\n}\n.dropdown-menu-filter---3sZdC {\n  margin: 8px;\n}\n.dropdown-menu---1pkAo .checkbox---1GXiZ > label {\n  width: 100%;\n}\ninput[type=\"text\"]:focus,\ninput[type=\"number\"]:focus,\ntextarea:focus,\n.input-focus---1oxQl {\n  border-color: #0096cc;\n  -webkit-transition: all 0.2s linear 0s;\n  -moz-transition: all 0.2s linear 0s;\n  -o-transition: all 0.2s linear 0s;\n  -ms-transition: all 0.2s linear 0s;\n  transition: all 0.2s linear 0s;\n  outline: none;\n}\n.input-icon-group---3IKaZ {\n  position: relative;\n}\n.input-icon-group---3IKaZ > .form-control---1sMvt {\n  padding-left: 30px;\n  padding-right: 36px;\n}\n.input-icon-group---3IKaZ > .form-control---1sMvt:focus + .input-icon-label---1ZNi_ > i,\n.input-icon-group---3IKaZ > .input-icon-label---1ZNi_ .focus---3VMuk {\n  -webkit-transition: all 0.2s linear 0s;\n  -moz-transition: all 0.2s linear 0s;\n  -o-transition: all 0.2s linear 0s;\n  -ms-transition: all 0.2s linear 0s;\n  transition: all 0.2s linear 0s;\n  color: #0096cc;\n}\n.input-icon-label---1ZNi_ {\n  position: absolute;\n  left: 9px;\n  top: 50%;\n  margin-top: -10px;\n  color: #666;\n}\n.form-control---1sMvt {\n  display: block;\n  width: 100%;\n  padding: 5px 12px;\n  font-size: 13px;\n  color: #222;\n  background-color: #fff;\n  background-image: none;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n}\n", ""]);
 
 // exports
 exports.locals = {
@@ -972,7 +972,9 @@ exports.locals = {
 	"form-control": "form-control---1sMvt",
 	"formControl": "form-control---1sMvt",
 	"input-icon-label": "input-icon-label---1ZNi_",
-	"inputIconLabel": "input-icon-label---1ZNi_"
+	"inputIconLabel": "input-icon-label---1ZNi_",
+	"focus": "focus---3VMuk",
+	"focus": "focus---3VMuk"
 };
 
 /***/ }),
@@ -27339,4 +27341,4 @@ if(false) {
 /***/ })
 
 /******/ });
-//# sourceMappingURL=bundle.js.map?9b524b2c94de348a4f45
+//# sourceMappingURL=bundle.js.map?036c57c5530c1ced45a8
