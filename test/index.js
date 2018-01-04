a478686f079eaa1f92fba8b6815f848c38ece2ae
Cheton Wu <cheton@gmail.com>
2018-01-04 11:23:24 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import Dropdown, { MenuItem } from '../src';
 
-test('noop', (t) => {
+test('<Dropdown />', (t) => {
+    const wrapper = mount((
+        <Dropdown>
+            <Dropdown.Toggle>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <MenuItem></MenuItem>
+                <MenuItem></MenuItem>
+                <MenuItem></MenuItem>
+            </Dropdown.Menu>
+        </Dropdown>
+    ));
+    t.equal(wrapper.find(Dropdown).length, 1, 'should render <Dropdown /> component');
+    t.equal(wrapper.find(Dropdown.Toggle).length, 1, 'should render <Dropdown.Toggle /> component');
+    t.equal(wrapper.find(Dropdown.Menu).length, 1, 'should render <Dropdown.Menu /> component');
+    t.equal(wrapper.find(MenuItem).length, 3, 'should render <MenuItem /> component');
     t.end();
 });
